0c09828b475cdae9c2ef05aec948cf8c4e78a0c4
Juan Nieto <juan.nieto-h@mail.escuelain.edu.co>
2026-08-21 21:31:39 -0500

    save/ save structure

--- src/main/java/edu/eci/dows/lab/Application.java
+++ src/main/java/edu/eci/dows/lab/Application.java
@@ -1,10 +1,5 @@
 package edu.eci.dows.lab;
 
-/**
- * Hello world!
- */
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+public class Application {
+    
 }
--- src/test/java/edu/eci/dows/lab/ApplicationTest.java
+++ src/test/java/edu/eci/dows/lab/ApplicationTest.java
@@ -4,14 +4,8 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest {
+public class ApplicationTest {
 
-    /**
-     * Rigorous Test :-)
-     */
     @Test
     public void shouldAnswerWithTrue() {
         assertTrue(true);
